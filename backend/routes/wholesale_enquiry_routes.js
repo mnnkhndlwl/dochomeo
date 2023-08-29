599e0bc0a510bb1548cd828a0dc561000b6964c0
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-08-29 15:35:25 +0530

    commit

--- backend/routes/wholesale_enquiry_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const equ = require("../controllers/WholeSaleEnquiry_Controller");
-
-router.post('/wholesaleEnquiry', equ.addWholeSaleEnquiry);
-router.get("/wholesaleEnquires/", equ.getWholeSaleEnquiries);
-router.get("/search/wholesale", equ.searchWholeSaleEnquiry);
-
-module.exports = router;
