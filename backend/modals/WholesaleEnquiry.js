30bfbc11cabb966eed4fc1c7edd352452ca72296
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-09-14 13:14:15 +0530

    quantity

--- backend/modals/WholesaleEnquiry.js
+++ backend/modals/WholesaleEnquiry.js
@@ -18,6 +18,9 @@ const wholeSaleEnquirySchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    quantity: {
+      type: Number,
+    },
     country: {
       type: String,
     },
